ddc485042f87915ceee004da5b1a07f5a5b7eb92
Aver roes <tamerabdalrazaq@hotmail.com>
2024-02-07 10:35:21 +0200

    C update

--- kmeans.c
+++ kmeans.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 
+EPSILON = 0.01
+
+    int
+    arrayLength(void *arr)
+{
+    return sizeof(arr) / sizeof(arr[0]);
+}
+
+double *createArray(double n)
+{
+    double *array = (double *)malloc(n * sizeof(double));
+    if (array == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        exit(1); // Exit program if memory allocation fails
+    }
+    return array;
+}
+
 double k_means(int K, int N, int d, int iter, int *data)
 {
     // create an array of k elements, initiated as the first k vektors
@@ -15,42 +34,100 @@ double k_means(int K, int N, int d, int iter, int *data)
     return 0;
 }
 
-int main(int argc , char* argv[])
+int main(int argc, char *argv[])
 {
-    //argc returns the count of arguments passed by the user including the program name
-    //argv is the array of strings after splitting the command line into arguments, including the program name
-    
+    // argc returns the count of arguments passed by the user including the program name
+    // argv is the array of strings after splitting the command line into arguments, including the program name
+
     return 0;
 }
 
+void k_means(double k, double n, double iter)
+{
+}
 
-void k_means(double k, double n, double iter ,
-double euc_12(double* v1, double* v2){
+double euc_12(double *v1, double *v2)
+{
     double dist = 0;
-    for (i=0 ; i<sizeof(v1) ;i++)
-        dist+=pow(v1[i]-v2[i],2.0)
-    return sqrt(dist) 
+    for (int i = 0; i < sizeof(v1); i++)
+        dist += pow(v1[i] - v2[i], 2.0);
+    return sqrt(dist);
 }
 
-//struct tuple_of_centXdist
+// struct tuple_of_centXdist
+// Forward declaration of struct Cluster
+struct Cluster;
+// Now you can declare variables of type struct Cluster pointer
+struct Cluster *centroids;
+struct Cluster
+{
+    double *centroid;
+    double size;
+};
 
-struct Cluster {
-    double* centroid;
-    int size;
-}
-
-double* find_closest_centroid(struct Cluster *vecs , double v[]){
+double *find_closest_centroid(struct Cluster *vecs, double v[])
+{
     double min_dist = 0.0;
     double *centroid;
-    for(i=0; i<sizeof(vecs); i++)
-        double d = euc_12(v,vecs[i]);
+    for (int i = 0; i < sizeof(vecs); i++)
+    {
+        double d = euc_12(v, vecs[i].centroid);
         if (d < min_dist)
+        {
             centroid = &vecs[i];
             min_dist = d;
-return centroid
-    
+        }
+    }
+    return centroid;
+}
 
+// def calc_centroid_average(centroid_size_tuple):
+//     centroid = centroid_size_tuple[0]
+//     size = centroid_size_tuple[1]
+//     return [x/size for x in centroid]
 
-    
-    
-        
+double *calc_centroid_average(struct Cluster cluster)
+{
+    double *centroid = cluster.centroid;
+    int n = arrayLength(centroid);
+    int size = cluster.size;
+    int *averaged_vector = createArray(n);
+    for (int i = 0; i < n; i++)
+    {
+        averaged_vector[i] = centroid[i] / size;
+    }
+    return averaged_vector;
+}
+
+// def check_centroid_convergence(centroids, new_centroids):
+//     convergent_centroids = 0
+//     for i in range(len(centroids)):
+//         if euc_l2(centroids[i], new_centroids[i]) <= EPSILON:
+//             convergent_centroids += 1
+//     return convergent_centroids == len(centroids)
+
+int check_centroid_convergence(struct Cluster *centroids, struct Cluster *new_centroids)
+{
+    int convergent_centroids = 0;
+    int k = arrayLength(centroids);
+    for (int i = 0; i < k; i++)
+    {
+        struct Cluster cluster_old = centroids[i];
+        struct Cluster cluster_new = new_centroids[i];
+        double *centroid_old = cluster_old.centroid;
+        double *centroid_new = cluster_new.centroid;
+        if (euc_l2(centroid_old, centroid_new) <= EPSILON)
+            convergent_centroids += 1;
+    }
+    return (convergent_centroids == k);
+}
+
+void add_vector_to_centroid(struct Cluster clus, double vec[])
+{
+    for (int i; i < sizeof(clus.centroid); i++)
+    {
+        double updated_entry_i = clus.centroid[i] + vec[i];
+        clus.centroid[i] = updated_entry_i;
+    }
+    clus.size++;
+}
