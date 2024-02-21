5b949cbd8a05050469d22d800cffbdb433956d3e
Aver roes <tamerabdalrazaq@hotmail.com>
2024-02-21 13:15:18 +0200

    fixed bugs in C file, program ready

--- kmeans.c
+++ kmeans.c
@@ -10,6 +10,13 @@
 #define ERROR_d "Invalid dimension of point!"
 #define ERROR_iter "Invalid maximum iteration!"
 
+struct Cluster
+{
+    double *centroid;
+    int size;
+};
+typedef struct Cluster Cluster;
+
 /*check if a string represents an integer*/
 int isStringDigit(const char *str)
 {
@@ -28,70 +35,17 @@ int isStringDigit(const char *str)
     return 1;
 }
 
-/*
-void printArray(double *arr, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        printf("%f ", (arr[i]));
-    }
-    printf("\n");
-}
-*/
-
-/*
-void print2DArray(double **array, int rows, int cols)
-{
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            printf("%lf ", (array[i][j]));
-        }
-        printf("\n");
-    }
-}
-*/
-
-struct Cluster
-{
-    double *centroid;
-    int size;
-};
-typedef struct Cluster Cluster;
-
 /*creates an array*/
 void *createArray(int n, int size)
 {
     void *array = malloc(n * size);
     if (array == NULL)
     {
-        printf("Memory allocation failed!\n");
-        exit(1); /* Exit program if memory allocation fails*/
+        printf("An Error Has Occurred");
+        exit(1);
     }
     return array;
 }
-
-/*
- *
-// void **create2DArray(int n, int d, int size)
-// {
-//     double **list[n][d];
-//     for (int i = 0; i < 5; i++)
-//     {
-//         for (int j = 0; j < 8; j++)
-//         {
-//             list[i][j] = (double **)malloc(sizeof(double *));
-//             if (list[i][j] == NULL)
-//             {
-//                 perror("Memory allocation failed");
-//                 return 1; // return error
-//             }
-//         }
-//     }
-//     return list;
-// }
-*/
 
 /*creates a submatrix*/
 double **sub_matrix_k(double **matrix, int k, int d)
@@ -122,13 +76,14 @@ void free_matrix(double **matrix, int k)
 }
 
 /*used to free memory*/
-void free_clusters(Cluster *clusters)
+void free_clusters(Cluster *clusters, int k)
 {
-    if (clusters != NULL)
+    int i;
+    for (i = 0; i < k; ++i)
     {
-        free(clusters->centroid);
-        free(clusters);
+        free(clusters[i].centroid);
     }
+    free(clusters);
 }
 
 /*calculates the euc distance between two given vectors*/
@@ -149,8 +104,8 @@ double euc_l2(double *v1, double *v2, int d)
 /*finds the closest cluster to a given vector by calculating the euc dist from it*/
 int find_closest_centroid_index(double **centroids, double *v, int k, int d)
 {
-    double min_dist = INFINITY;
-    int index;
+    double min_dist = euc_l2(v, centroids[0], d);
+    int index = 0;
     int i;
     for (i = 0; i < k; i++)
     {
@@ -212,18 +167,17 @@ void add_vector_to_centroid(Cluster *clus, double const vec[], int d)
     clus->size = clus->size + 1;
 }
 
-
 double **k_means(int k, int n, int d, int iter, double **data)
 {
     /*creates the initial centroids according to the first k vectors given in the input*/
     int i, j, data_i, convergence, closest_centroid_index;
     double **centroids, **updated_centroids;
     double *x;
-    struct Cluster *new_centroids;
+
     centroids = sub_matrix_k(data, k, d);
     for (i = 0; i < iter; i++)
     {
-        new_centroids = (struct Cluster *)createArray(k, sizeof(Cluster));
+        struct Cluster *new_centroids = (struct Cluster *)createArray(k, sizeof(Cluster));
         for (j = 0; j < k; j++)
         {
             new_centroids[j].size = 0;
@@ -241,6 +195,8 @@ double **k_means(int k, int n, int d, int iter, double **data)
             updated_centroids[j] = calc_centroid_average(new_centroids[j], d);
         }
         convergence = check_centroid_convergence(updated_centroids, centroids, k, d);
+        free_matrix(centroids, k);
+        free_clusters(new_centroids, k);
         centroids = updated_centroids;
         if (convergence)
             break;
@@ -256,17 +212,17 @@ int main(int argc, char *argv[])
     if (isStringDigit((char *)argv[3]) == 0)
     {
         printf(ERROR_d);
-        return 0;
+        return 1;
     }
     if (isStringDigit((char *)argv[1]) == 0)
     {
         printf(ERROR_K);
-        return 0;
+        return 1;
     }
     if (isStringDigit((char *)argv[2]) == 0)
     {
         printf(ERROR_N);
-        return 0;
+        return 1;
     }
     K = strtol(argv[1], &end1, 10);
     n = strtol(argv[2], &end2, 10);
@@ -286,12 +242,13 @@ int main(int argc, char *argv[])
             data[i][j] = m;
         }
     }
-    if (argc >= 6)
+    if (argc >= 5)
     {
         if (isStringDigit((char *)argv[4]) == 0)
         {
             printf(ERROR_iter);
-            return 0;
+            free_matrix(data, n);
+            return 1;
         }
         else
         {
@@ -302,21 +259,29 @@ int main(int argc, char *argv[])
     {
         iter = ITER;
     }
-
     if (n <= 1)
     {
         printf(ERROR_N);
-        return 0;
+        free_matrix(data, n);
+        return 1;
     }
     if (K < 1 || n < K)
     {
         printf(ERROR_K);
-        return 0;
+        free_matrix(data, n);
+        return 1;
+    }
+    if (d < 1)
+    {
+        printf(ERROR_d);
+        free_matrix(data, n);
+        return 1;
     }
     if (iter < 1 || iter > 1000)
     {
         printf(ERROR_iter);
-        return 0;
+        free_matrix(data, n);
+        return 1;
     }
 
     output = k_means(K, n, d, iter, data);
@@ -325,54 +290,14 @@ int main(int argc, char *argv[])
     {
         for (j = 0; j < d; j++)
         {
-            printf("%.4f ", output[i][j]);
+            printf("%.4f", output[i][j]);
+            if (j < d - 1)
+                printf(",");
         }
         printf("\n");
     }
+    free_matrix(data, n);
+    free_matrix(output, K);
+
     return 0;
 }
-
-/*int main()
-{
-    int k = 4;
-    int n = 6;
-    int d = 2;
-    int iter = 3;
-    int rows = 6;
-    int cols = 2;
-    printf("1111111111111111111111 \n");
-    // Dynamically allocate memory for the 2D array
-    //double **array = malloc(rows * sizeof(double *));
-    //for (int i = 0; i < rows; i++)
-    //{
-    //    array[i] = malloc(cols * sizeof(double));
-    //}
-
-    double **array = (double **)createArray(rows, sizeof(double *));
-    int i;
-    for (i = 0; i < rows; i++)
-    {
-        array[i] = (double *)createArray(cols, sizeof(double));
-    }
-    //[(1, 2), (5, 8), (1, 5), (6, 9), (2, 2), (8, 1)]
-    array[0][0] = 1;
-    array[0][1] = 2;
-    array[1][0] = 5;
-    array[1][1] = 8;
-    array[2][0] = 1;
-    array[2][1] = 5;
-    array[3][0] = 6;
-    array[3][1] = 9;
-    array[4][0] = 2;
-    array[4][1] = 2;
-    array[5][0] = 8;
-    array[5][1] = 1;
-
-    //print2DArray(array, rows, cols);
-
-    printf("hiiiiiiiiiiiarraiiiiiiiiiiiiiiiiiiiiii\n");
-    k_means(k, n, d, iter, array);
-    printf("3333333333333333\n");
-    return 0;
-}
-*/
